7e510356705ac051c3843c079cda92e7e2868b52
Nimaturkmani <niamatullah.turkmani@gmail.com>
2023-12-09 04:18:38 +0100

    Add files via upload
    New changes

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
-$(document). ready(function(){
-	setBindings();
-	
+$(document).ready(function(){ //ensures that the script runs only 
+							//after the HTML document has been fully loaded.
+    setBindings();
 });
 
 function setBindings() {
-	$("nav a").click(function(e){
-		e.preventDefault();
-		var sectionID = e.currentTarget.id + 
-			"Section";
-		$("html body").animate({
-			scrollTop: $("#" + sectionID).offset()
-			.top
-			
-		}, 1000)
-	})
+    $("nav a").click(function(e){ //This selects all anchor elements inside the "nav" element 
+									//and attaches a click event handler.
+        e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+        $("html, body").animate({
+            scrollTop: $("#" + sectionID).offset().top
+        }, 600);
+    });
 }
